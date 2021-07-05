c60e32402a82ebdebec610b5b0671803117e19bf
Cris Jr <crisjr@pm.me>
2021-07-05 16:36:16 -0300

    Set scramble result based on API

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -6,16 +6,23 @@
 (defn get-field [field-id]
     (.. (dom/getElement field-id) -value))
 
-(defn on-click [event]
-    (let [str1 (get-field "str1")
-          str2 (get-field "str2")
-          url (str "/api/scramble?str1=" str1 "&str2=" str)
-          result (GET url)]
-        (js/console.log result))
-)
+(defn set-field [field-id inner-html]
+    (-> js/document
+        (.getElementById field-id)
+        (.-innerHTML)
+        (set! inner-html)))
 
-(events/listen
-    (dom/getElement "scramble-button")
-    "click"
-    on-click
-)
+(defn on-click-handler [result]
+    (set-field "scramble-result" (if result "Yes!" "No!")))
+
+(defn on-click-error-handler [error]
+    (js/console.log error))
+
+(defn on-click [event]
+    (GET "/api/scramble" {:params {:str1 (get-field "str1")
+                                   :str2 (get-field "str2")}
+                          :handler on-click-handler
+                          :error-handler on-click-error-handler
+                          :response-format :json}))
+
+(events/listen (dom/getElement "scramble-button") "click" on-click)
