f1ed9206f1a4327d8a28dc09286bf8ab11c5ff1f
Cris Jr <crisjr@pm.me>
2021-07-05 16:49:31 -0300

    Update configuration files

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
               :url "https://www.eclipse.org/legal/epl-2.0/"}
     :dependencies [[org.clojure/clojure "1.10.1"]
                    [org.clojure/clojurescript "1.10.758"]
-                   ;clojure deps
+                   ; clojure deps
                    [compojure "1.6.1"]
                    [http-kit "2.3.0"]
                    [ring/ring-defaults "0.3.2"]
